877117962ceafe2c19fd23d8ba0d910ff8aede39
Cory Willmon <corywillmon123@gmail.com>
2021-06-01 12:19:34 -0500

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 //Imports pages
 import login from './pages/login'
